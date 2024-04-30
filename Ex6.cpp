0285a8e377c7f5ab57dcd5909f9b9965dc921956
FRANCO2022015 <112721991+FRANCO2022015@users.noreply.github.com>
2024-04-30 14:09:17 -0500

    Update Ex6.cpp

--- Ex6.cpp
+++ Ex6.cpp
@@ -1,72 +1,85 @@
 //
+// Created by Aarón Timaná on 29/04/2024.
+//
+//
 // Created by Franco on 29/04/2024.
 //
 
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
 struct Node{
     int data;
     Node* next;
+    //Node (int, Node*) : data(0), next(nullptr){};
 };
 
 class list{
 private:
     Node* head;
 public:
-    list(): head(nullptr){}
+    list(): head(nullptr){};
+
     void push_front(int valor){
-        Node* NewNode = new Node(valor);
-        NewNode ->next = head;
+        //Node* NewNode = new Node(valor, nullptr);
+        Node* NewNode = new Node;
+        NewNode->data=valor;
+        NewNode->next = head;
         head = NewNode;
     }
 
-    void bubblesort(list& lista_ordenada) {
-        if(lista_ordenada.head == nullptr || lista_ordenada.head->next == nullptr){
+    void bubblesort() {
+        if(head == nullptr || head->next == nullptr){
             return;
+        } else {
+            Node* ptr1, *ptr2;
+            int temp;
+            for(ptr1 = head; ptr1->next!= nullptr;ptr1=ptr1->next){
+                for(ptr2 = ptr1->next; ptr2!= nullptr;ptr2=ptr2->next){
+                    if(ptr1->data > ptr2->data){
+                        temp = ptr1->data;
+                        ptr1->data = ptr2->data;
+                        ptr2->data=temp;
+                    }
+                }
+            }
         }
-        bool swapped = true;
-
-        Node* current;
-
-        while ()
     }
     void printeo() {
+        cout<<"\nLista ordenada xd:"<<endl;
         Node* current = head;
         while (current != nullptr) {
             cout << current->data << " ";
             current = current->next;
         }
-        cout << endl;
+    }
+
+    ~list(){
+        Node* temp;
+        while (head!= nullptr){
+            temp=head;
+            head=head->next;
+            delete temp;
+        }
     }
 
 };
-/*
-void bubblesort(vector<int>& vec_orde){
-    int n = vec_orde.size();
-    for(int i=0; i<n-1; i++){
-        for(int j=0; j<=n-1; j++){
-            if(vec_orde[j]>vec_orde[j+1]){
-                swap(vec_orde[j], vec_orde[j+1]);
-            }
-        }
-    }
+
+int main() {
+    list list1;
+    list1.push_front(6);
+    list1.push_front(7);
+    list1.push_front(5);
+    list1.push_front(1);
+    list1.push_front(20);
+    list1.push_front(0);
+
+    list1.printeo();
+    //
+    list1.bubblesort();
+    list1.printeo();
+
+    return 0;
 }
-
-int main(){
-    vector<int> vec = {64,25,12,22,11};
-    for(int num:vec){
-        cout<<num<<" ";
-    }
-    cout<<endl;
-
-    bubblesort(vec);
-    for(int num:vec){
-        cout<<num<<" ";
-    }
-    cout<<endl;
-
-}*/
